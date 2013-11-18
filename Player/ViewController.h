0c42dad7590550f6fa1536d13c818a4cdf375c74
nullproduction <nullproduction@gmail.com>
2013-11-18 13:44:59 +0400

    Initial Commit

---
+++ Player/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Player
+//
+//  Created by mezhevikin on 18.11.13.
+//  Copyright (c) 2013 Mezhevikin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PlayerTests/PlayerTests.m
@@ -0,0 +1,34 @@
+//
+//  PlayerTests.m
+//  PlayerTests
+//
+//  Created by mezhevikin on 18.11.13.
+//  Copyright (c) 2013 Mezhevikin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PlayerTests : XCTestCase
+
+@end
+
+@implementation PlayerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
